58f03752a54574fe019f09c73257cf4e13449c47
edoardo <dipaolo.1728334@studenti.uniroma1.it>
2021-07-21 15:58:09 +0200

    Update routing

--- model/flood-wup-routing.cc
+++ model/flood-wup-routing.cc
@@ -1,1 +1,35 @@
 // routing
+
+
+#include "flood-wup-routing.h"
+#include "flood-wup-wakeup-packet.h"
+
+namespace ns3 {
+    NS_LOG_COMPONENT_DEFINE("FloodWUPRouting");
+    NS_OBJECT_ENSURE_REGISTERED(FloodWUPRouting);
+
+    FloodWUPRouting::FloodWUPRouting() {
+        m_sentPktCount = 0;
+    }
+
+    TypeId FloodWUPRouting::GetTypeId() {
+        static TypeId tid = TypeId("ns3:FloodWUPRouting")
+            .SetParent<Object> ()
+            .AddConstructor<FloodWUPRouting> ();
+        
+        return tid;
+    }
+
+    Ptr<FloodWUPWakeupPacket> FloodWUPRouting::CreateWakeUpPacket() {
+        Ptr<FloodWUPWakeupPacket> pkt = Create<FloodWUPWakeupPacket>();
+
+        if(pkt == NULL)
+            return NULL;
+
+        //pkt->SetWakeUpSequence() // TODO: setto la wakeup sequence
+        // pkt->SetSequenceNumber() // TODOO: setto il sequence number. 
+        
+        m_sentPktCount += 1; // incremento i pacchetti inviati.
+        return pkt;
+    }
+}
--- model/flood-wup-routing.h
+++ model/flood-wup-routing.h
@@ -1,1 +1,42 @@
 // routing
+
+#ifndef FLOOD_WUP_ROUTING
+#define FLOOD_WUP_ROUTING
+
+#include "ns3/object.h"
+#include "ns3/packet.h"
+#include "flood-wup-wakeup-packet.h"
+
+namespace ns3 {
+
+    class FloodWUPRouting : public Object {
+
+        public:
+            static TypeId GetTypeId(void);
+            FloodWUPRouting(void);
+            virtual ~FloodWUPRouting(void);
+
+            virtual void SetNetDevice(); // TODO: collegamento col NetDevice
+            virtual void SetMac(); // TODO: MAC layer
+
+            int SentPktCount() const;
+
+        protected:
+            virtual bool IsDst(const Ptr<Packet> p) const;
+            virtual bool IsSrc(const Ptr<Packet> p) const;
+            Ptr<FloodWUPWakeupPacket> CreateWakeUpPacket();
+            virtual void SendPacket(Ptr<Packet> p);
+            // TODO: getnetdevice e getmac
+
+            virtual void DoDispose();
+
+            int m_sentPktCount;
+
+
+        
+    };
+
+
+}
+
+#endif
