2971d5f0f1e5074358f7764b3d5e8732d4887a8d
Edoardo Di Paolo <dipaolo.1728334@studenti.uniroma1.it>
2021-08-26 16:24:56 +0200

    Added wake-up sequence packet header

---
+++ model/flood-wakeup-packet.h
@@ -0,0 +1,31 @@
+#ifndef FLOOD_WAKEUP_PACKET
+#define FLOOD_WAKEUP_PACKET
+
+#include "ns3/header.h"
+#include "ns3/mac16-address.h"
+
+namespace ns3 {
+    class FloodWUPPacketHeader : public Header {
+
+        public:
+            FloodWUPPacketHeader() {};
+            virtual ~FloodWUPPacketHeader() {};
+            static TypeId GetTypeId(void);
+            virtual TypeId GetInstanceTypeId(void) const;
+            virtual uint32_t GetSerializedSize(void) const;
+            virtual void Serialize(Buffer::Iterator start) const;
+            virtual uint32_t Deserialize(Buffer::Iterator start);
+            virtual void Print(std::ostream &os) const;
+            void SetHeaderWakeUpSequence(Mac16Address seq) {
+                wakeUpSequence = seq;
+            };
+            Mac16Address GetWakeUpSequence() {
+                return wakeUpSequence
+            };
+
+        private:
+            Mac16Address wakeUpSequence;
+    };
+}
+
+#endif
